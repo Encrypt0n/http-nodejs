15e970d71d46d061092b0fbc963713b7554cc021
Encrypt0n <37348021+Encrypt0n@users.noreply.github.com>
2023-06-12 12:21:36 +0200

    Add files via upload

---
+++ app.mjs
@@ -0,0 +1,94 @@
+/*const express = require('express');
+
+const NodeMediaServer = require('node-media-server');
+const cors = require('cors');
+const { Readable } = require('stream');
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffmpeg = require('fluent-ffmpeg');*/
+import express from 'express';
+import NodeMediaServer from 'node-media-server';
+import cors from 'cors';
+import {Readable} from 'stream';
+import ffmpegPath from '@ffmpeg-installer/ffmpeg';
+import ffmpeg from 'fluent-ffmpeg';
+import { createServer } from 'https';
+import { readFileSync } from 'fs';
+import { WebSocketServer } from 'ws';
+
+
+
+ffmpeg.setFfmpegPath(ffmpegPath);
+
+const app = express();
+app.use(cors());
+
+const streamName = 'myStream';
+const rtmpUrl = `rtmp://localhost/live/${streamName}`;
+
+// Configure the Node Media Server
+const config = {
+  rtmp: {
+    port: 1935,
+    chunk_size: 4096,
+    gop_cache: false,
+    ping: 1,
+    ping_timeout: 60,
+  },
+  http: {
+    port: 8001,
+    mediaroot: './media',
+    allow_origin: '*',
+  },
+};
+
+// Create an instance of the Node Media Server
+const nms = new NodeMediaServer(config);
+
+// Start the Node Media Server
+nms.run();
+
+// Serve the client-side code
+app.use(express.static('public'));
+
+// WebSocket server
+//const WebSocket = require('ws');
+const server = createServer(app);
+const wss = new WebSocketServer({ server  });
+
+wss.on('connection', (ws) => {
+  const videoStream = new Readable();
+  videoStream._read = () => {};
+
+  const camera = ffmpeg()
+    .input(videoStream)
+    //.inputFormat('h264')
+    .inputOptions('-re')
+    .outputOptions('-c:v copy')
+    .outputOptions('-f flv')
+    .output(rtmpUrl);
+
+  console.log('Streaming started');
+  camera.run();
+
+  camera.on('end', () => {
+    console.log('Streaming finished');
+  });
+
+  camera.on('error', (error) => {
+    console.error('Error streaming video:', error);
+  });
+
+  ws.on('message', (message) => {
+    videoStream.push(message);
+  });
+
+  ws.on('close', () => {
+    videoStream.destroy();
+    camera.kill();
+  });
+});
+
+// Start the server
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
